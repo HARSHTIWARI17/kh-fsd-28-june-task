5acddfe0431931eb5efffc5ccd1b32763e5a4ad0
HARSH TIWARI <Harsh tiwari>
2022-06-29 17:24:46 +0530

    absd

---
+++ JUNE-22/june-23/june-28/index.js
@@ -0,0 +1,68 @@
+//console.log(1+2);
+//console.log("1"+"2");
+
+//console.log(1+1+"2");
+//console.log("3" - "1");
+
+//console.log(2+3+"4" -10+5);
+
+
+//let num=8;
+  //if (num1%2==0){
+    //   console.log("even number")
+  //}
+ //else console.log("odd number");
+
+//let number =20
+ //while (number >=1);
+// {
+   // console.log(number)
+    //n//um=num-1;
+    
+ 
+
+// let Number=105;
+ //let remainder = number % 2;
+ 
+ //if (remainder == 0){ console.log(number + "even number")
+//} else{
+  // console.log(number + "odd number");
+//}
+
+let Number =63;
+if ( Number % 15 ==0){
+   console.log("fizzbuzz");
+}
+else if ( Number % 3 ==0 ){
+   console.log("fizz");
+} else if (Number % 5 ==0){
+   console.log("buzz");
+}
+else {
+   console.log(Number);
+}
+
+//let number =1;
+ //while (number< 10){
+   //console.log(number);
+   //number=number+1;
+ //}
+
+ //let number = 20;
+ //while (number>0) {
+   //console.log(number);
+   //number = number-1;
+ //}
+
+ let prime = 97;
+ if (prime  % 2 ==0){
+   console.log("this is not a prime number")
+ } else {
+   console.log("this is prime number");
+ }
+
+
+
+
+
+
